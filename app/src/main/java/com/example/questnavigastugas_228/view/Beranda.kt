c5007904fcb8aa2d85120693fefc69701bebca52
MannantaB <mannanta18@gmail.com>
2025-11-07 15:56:27 +0700

    add lilac background and scaffold layout

--- app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
@@ -1,13 +1,33 @@
 package com.example.questnavigastugas_228.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
     OnSubmitBtnClick:()-> Unit
 ){
-
+    Scaffold { isiRuang->
+        val jeno = painterResource(R.drawable.jeno)
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color(0xFFD8B4FE))
+                .padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){  }
+    }
 }
