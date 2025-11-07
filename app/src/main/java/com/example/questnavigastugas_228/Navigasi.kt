5abebeb4bb508da58d0d4686a15850e9831e351b
MannantaB <mannanta18@gmail.com>
2025-11-07 17:28:24 +0700

    final perbaikan Navigasi.kt

---
+++ app/src/main/java/com/example/questnavigastugas_228/Navigasi.kt
@@ -0,0 +1,75 @@
+package com.example.questnavigastugas_228
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas_228.view.DaftarPeserta
+import com.example.questnavigastugas_228.view.FormDataDiri
+import com.example.questnavigastugas_228.view.HomePage
+
+enum class Navigasi {
+    Beranda,
+    Formulir,
+    List
+}
+
+@Composable
+fun HomeApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Beranda.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Beranda.name) {
+                HomePage(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.List.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.List.name) {
+                DaftarPeserta(
+                    onBackToBerandaClick = {
+                        cancelAndBackToBeranda(navController)
+                    },
+                    OnBtnToFormulirClick = {
+                        navController.navigate(Navigasi.Formulir.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Formulir.name) {
+                FormDataDiri(
+                    onBackToBtnClick = {
+                        cancelAndBackToList(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToBeranda(navController: NavController) {
+    navController.popBackStack(
+        route = Navigasi.Beranda.name,
+        inclusive = false
+    )
+}
+
+private fun cancelAndBackToList(navController: NavController) {
+    navController.popBackStack(
+        route = Navigasi.List.name,
+        inclusive = false
+    )
+}
