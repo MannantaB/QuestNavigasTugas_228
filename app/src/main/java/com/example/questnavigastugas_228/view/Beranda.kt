c8928794d47c647128cd7e5f28ff8ce9fb59df4a
MannantaB <mannanta18@gmail.com>
2025-11-07 16:01:52 +0700

    add styled welcome text

--- app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
@@ -7,11 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -28,6 +34,13 @@ fun HomePage(
                 .padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
-        ){  }
+        ){
+            Text(text= stringResource(id = R.string.welcome), modifier = Modifier,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                fontSize = 40.sp,
+                color = Color(0xFFAD6FBF)
+            )
+        }
     }
 }
