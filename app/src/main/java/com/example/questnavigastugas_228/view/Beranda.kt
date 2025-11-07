1a35bdcb95432a474e58f3cdcee4a20f396e83b5
MannantaB <mannanta18@gmail.com>
2025-11-07 16:09:12 +0700

    add styled name text

--- app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas_228.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -45,6 +47,17 @@ fun HomePage(
                 color = Color(0xFFAD6FBF)
             )
             Spacer(modifier = Modifier.height(height = 30.dp))
+            Image(painter=jeno,
+                contentDescription = null,
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+            Text(text = stringResource(id=R.string.Nama),
+                modifier = Modifier,
+                fontSize = 22.sp,
+                fontFamily = FontFamily.Monospace,
+                color = Color(0xFFAD6FBF)
+            )
         }
     }
 }
