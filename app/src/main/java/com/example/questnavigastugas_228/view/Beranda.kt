905d0dcaf712792197f109dcc69477050e26b42b
MannantaB <mannanta18@gmail.com>
2025-11-07 15:45:36 +0700

    add OnSubmitBtnClick callback parameter

--- app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_228/view/Beranda.kt
@@ -1,10 +1,13 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
 package com.example.questnavigastugas_228.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(
+    OnSubmitBtnClick:()-> Unit
+){
 
-){}
+}
